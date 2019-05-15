4a8fc6b75118978bcc42ff0f60f14cc8f7805c45
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-05-15 17:54:12 +0200

    update via lan

---
+++ controller/lib/controller/clock/clock.ex
@@ -0,0 +1,29 @@
+defmodule Controller.Clock do
+  use GenServer
+
+  require Logger
+
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_) do
+    do_job()
+    schedule_work()
+    {:ok, nil}
+  end
+
+  def handle_info(:collect_orders, _) do
+    do_job()
+    schedule_work()
+    {:noreply, nil}
+  end
+
+  def do_job do
+    IO.puts("HI!#{NaiveDateTime.utc_now()}")
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :collect_orders, 100)
+  end
+end
