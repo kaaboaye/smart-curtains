3cf0482f2b66ee56368f6d4eecd2c1cf5d922686
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-05-23 19:39:05 +0200

    refactor routing

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Tasks from "./views/Tasks.vue";
+import Task from "./views/Task.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -14,22 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: "/edit",
-      name: "aedit",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Edit.vue")
+      path: "/tasks",
+      name: "tasks",
+      component: Tasks
     },
     {
-      path: "/view",
-      name: "view",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/View.vue")
+      path: "/task/:taskId",
+      name: "task",
+      component: Task
     }
   ]
 });
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    tasks: []
+  },
+  mutations: {
+    addTask: (state, { task }) => state.tasks.push(task)
+  },
   actions: {}
 });
